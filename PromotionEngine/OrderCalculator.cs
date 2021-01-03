98033f93aa86e1ae8a9affd7f0438b391f46ccbb
Chandrakant <Chandrakant.verma94@live.in>
2021-01-03 08:39:45 +0530

    Main class added for calculation

---
+++ PromotionEngine/OrderCalculator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    public class OrderCalculator
+    {
+        private readonly IPromotionEngine _promotionEngine;
+
+        /// <summary>
+        /// Inject dependency of IPromotionEngine
+        /// </summary>
+        /// <param name="PromotionEngine"> Promotion engine logic instance</param>
+        public OrderCalculator(IPromotionEngine PromotionEngine)
+        {
+            _promotionEngine = PromotionEngine;
+        }
+
+        /// <summary>
+        /// Pass Purchased cart
+        /// </summary>
+        /// <param name="cart">Cart having master and purchased data</param>
+        /// <returns></returns>
+        public double CalculatePrice(SkuCart cart)
+        {
+            return _promotionEngine.GetOrderPrice(cart);
+        }
+    }
+}
