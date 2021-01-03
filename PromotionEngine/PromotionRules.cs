9ac8d6c4bc86bece4ae6b18c0eb8f6f323615245
Chandrakant <Chandrakant.verma94@live.in>
2021-01-03 08:31:48 +0530

    Promotion Rules Added

---
+++ PromotionEngine/PromotionRules.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PromotionEngine
+{
+    class PromotionRules
+    {
+        public static IDictionary<char, Tuple<int, double>> _rules;
+
+        public static IList<Tuple<char, char, double>> _comboRules;
+
+        public PromotionRules(IDictionary<char, Tuple<int, double>> Rules, IList<Tuple<char, char, double>> ComboRules)
+        {
+            _rules = Rules;
+            _comboRules = ComboRules;
+        }
+        public static Tuple<int, double> GetRule(char SKUId)
+        {
+            if (_rules.ContainsKey(SKUId))
+                return _rules[SKUId];
+            else
+                return null;
+        }
+    }
+}
